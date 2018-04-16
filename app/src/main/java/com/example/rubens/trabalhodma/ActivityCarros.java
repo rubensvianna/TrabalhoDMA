fd3ea9cae5fa2ba9c5d37a6a4d0bbf703e8b8780
SocialLifeProgrammer <sociallifeprogrammer@gmail.com>
2018-04-16 20:57:33 -0300

    Atualizando projeto

--- app/src/main/java/com/example/rubens/trabalhodma/ActivityCarros.java
+++ app/src/main/java/com/example/rubens/trabalhodma/ActivityCarros.java
@@ -18,6 +18,7 @@ public class ActivityCarros extends AppCompatActivity {
 
         lvCarros = (ListView) findViewById(R.id.listViewCarros);
         List<Carros> listaCarros = new ArrayList<>();
+
         listaCarros.add(new Carros(1L, "Montana", "Chevrolet", "1.4 LS 8V Flex 2P Manual",
                 "2018/2018", "R$ 38.291,00", "(44) 999655469", R.drawable.montana));
 
@@ -29,6 +30,35 @@ public class ActivityCarros extends AppCompatActivity {
 
         listaCarros.add(new Carros(4L, "Grand Siena", "Fiat", "1.4 Attractive 8V 85CV 4P Flex Manual",
                 "2013/2014", "R$ 35.990,00", "(44) 999655469", R.drawable.siena));
+
+        listaCarros.add(new Carros(5L, "Range Rover","RangeRover","2.8 Atractive",
+                "2016/2017","420.987,00","(44)3482-0110",R.drawable.rangrover01));
+
+        listaCarros.add(new Carros(7L, "Ferrari 2018","Ferrari","3.0 UltraSpeed",
+                "2017/2018","987.291,00","(44)3482-0110",R.drawable.ferrari));
+
+        listaCarros.add(new Carros(8L, "Range Rover 2018","RangeRover","3.0 UltraSpeed",
+                "2017/2018","782.987,00","(44)3482-0110",R.drawable.rangrover02));
+
+        listaCarros.add(new Carros(9L, "Lamborguinhi 2018","Lamborguinhi","3.0 Ultra Veloz",
+                "2018/2019","3.298.983,00","(44)3482-0110",R.drawable.lamborguinhi));
+
+        listaCarros.add(new Carros(10L, "Lamborguinhi 2018","Lamborguinhi","3.0 Ultra Veloz",
+                "2018/2019","3.298.983,00","(44)3482-0110",R.drawable.lamborguini01));
+
+        listaCarros.add(new Carros(11L, "MCLAREN 2018","MCLaren","2.9Veloz",
+                "2018/2019","7.298.983,00","(44)3482-0110",R.drawable.mclaren));
+
+        listaCarros.add(new Carros(12L, "Lamborguinhi 2018","Lamborguinhi","3.0 Ultra Veloz",
+                "2018/2019","2.298.983,00","(44)3482-0110",R.drawable.mclaren01));
+
+
+        listaCarros.add(new Carros(13L, "Porshe 2018","Lamborguinhi","3.0 Ultra Veloz",
+                "2018/2019","1.298.983,00","(44)3482-0110",R.drawable.porshe));
+
+        listaCarros.add(new Carros(14L, "Porshe 2018","Lamborguinhi","3.0 Ultra Veloz",
+                "2018/2019","327.983,00","(44)3482-0110",R.drawable.marca_Porshe));
+
 
         AdapterCarros adapter = new AdapterCarros(this, listaCarros);
         lvCarros.setAdapter(adapter);
