a73e5958794b17841f956794ec6ee2bc4b1a1176
SocialLifeProgrammer <sociallifeprogrammer@gmail.com>
2018-04-18 10:21:12 -0300

    Inserção de novos veículos para venda

--- app/src/main/java/com/example/rubens/trabalhodma/ActivityCarros.java
+++ app/src/main/java/com/example/rubens/trabalhodma/ActivityCarros.java
@@ -32,28 +32,28 @@ public class ActivityCarros extends AppCompatActivity {
                 "2013/2014", "R$ 35.990,00", "(44) 999655469", R.drawable.siena));
 
         listaCarros.add(new Carros(5L, "Range Rover","RangeRover","2.8 Atractive",
-                "2016/2017","420.987,00","(44)3482-0110",R.drawable.rangrover01));
+                "2016/2017","R$420.987,00","(44)3482-0110",R.drawable.rangrover01));
 
         listaCarros.add(new Carros(7L, "Ferrari 2018","Ferrari","3.0 UltraSpeed",
-                "2017/2018","987.291,00","(44)3482-0110",R.drawable.ferrari));
+                "2017/2018","R$987.291,00","(44)3482-0110",R.drawable.ferrari));
 
         listaCarros.add(new Carros(8L, "Range Rover 2018","RangeRover","3.0 UltraSpeed",
-                "2017/2018","782.987,00","(44)3482-0110",R.drawable.rangrover02));
+                "2017/2018","R$782.987,00","(44)3482-0110",R.drawable.rangrover02));
 
         listaCarros.add(new Carros(9L, "Lamborguinhi 2018","Lamborguinhi","3.0 Ultra Veloz",
-                "2018/2019","3.298.983,00","(44)3482-0110",R.drawable.lamborguinhi));
+                "2018/2019","R$3.298.983,00","(44)3482-0110",R.drawable.lamborguinhi));
 
         listaCarros.add(new Carros(10L, "Lamborguinhi 2018","Lamborguinhi","3.0 Ultra Veloz",
-                "2018/2019","3.298.983,00","(44)3482-0110",R.drawable.lamborguini01));
+                "2018/2019","R$3.298.983,00","(44)3482-0110",R.drawable.lamborguini01));
 
         listaCarros.add(new Carros(11L, "MCLAREN 2018","MCLaren","2.9Veloz",
-                "2018/2019","7.298.983,00","(44)3482-0110",R.drawable.mclaren));
+                "2018/2019","R$7.298.983,00","(44)3482-0110",R.drawable.mclaren));
 
         listaCarros.add(new Carros(12L, "Lamborguinhi 2018","Lamborguinhi","3.0 Ultra Veloz",
-                "2018/2019","2.298.983,00","(44)3482-0110",R.drawable.mclaren01));
+                "2018/2019","R$2.298.983,00","(44)3482-0110",R.drawable.mclaren01));
 
 
-        listaCarros.add(new Carros(13L, "Porshe 2018","Lamborguinhi","3.0 Ultra Veloz",
+        listaCarros.add(new Carros(13L, "Porshe 2018","Porshe","3.0 Ultra Veloz",
                 "2018/2019","1.298.983,00","(44)3482-0110",R.drawable.porshe));
 
 
